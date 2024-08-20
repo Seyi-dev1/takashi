5ac0c257e1e8a83da62b1f3ab25834ac7f668781
Ahia kleine chris <chriskleine433@gmail.com>
2024-08-20 16:34:09 +0100

    made new sections

--- src/pages/home/pages/Homepage/Homepage.jsx
+++ src/pages/home/pages/Homepage/Homepage.jsx
@@ -1,5 +1,19 @@
+import SectionOne from "../../../../components/section-1/SectionOne";
+import SectionTwo from "../../../../components/section-2/SectionTwo";
+import SectionThree from "../../../../components/section-3/SectionThree";
+import SectionFour from "../../../../components/section-4/SectionFour";
+import SectionFive from "../../../../components/section-5/SectionFive";
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <SectionOne />
+      <SectionTwo />
+      <SectionThree />
+      <SectionFour />
+      <SectionFive />
+    </div>
+  );
 };
 
 export default Homepage;
